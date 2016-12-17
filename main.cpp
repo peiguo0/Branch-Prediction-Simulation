2c3a020f99e415336fa3e128137ba8071eaaf5e6
JamesG3 <emislim12@gmail.com>
2016-12-17 00:50:54 -0500

    update

--- main.cpp
+++ main.cpp
@@ -11,8 +11,9 @@
 
 using namespace std;
 //access state:
-#define TAKEN  0 // no action
-#define NOT TAKEN 1 // read hit
+#define TAKEN 1
+#define NOTTAKEN 0
+
 
 
 
@@ -20,6 +21,8 @@ using namespace std;
 int main(){
     
     int M=0;
+    
+
 
     ifstream cache_params;
     //cache_params.open(argv[1]);
@@ -30,7 +33,10 @@ int main(){
         cache_params>>M;
     }
     
+    vector<bitset<2> > saturating_counter;
+    saturating_counter.assign(pow(2, M) , 3);       //initialized every counter to 11
     
+
     
     // Implement by you:
 
@@ -64,15 +70,51 @@ int main(){
             saddr >> std::hex >> addr;
             accessaddr = bitset<32> (addr);
             
-            std::cout<<accessaddr;
-            std::cout<<"\n";
-            std::cout<<TakenResult;
-            std::cout<<"\n";
+            bitset<4> temIndex;
+            long PredictIndex=0;
+            int index=0;        //used for count the 4 LSBs
             
+            while (index!=4) {
+                temIndex[index] = accessaddr[index];
+                index+=1;
+            }
             
+            PredictIndex = temIndex.to_ulong();
 
+            //saturating counter reading, to get the prediction of current branch
+            if (saturating_counter[PredictIndex] == 0 or saturating_counter[PredictIndex] == 1){
+                TakenPrediction = NOTTAKEN;
+            }
+            
+            if (saturating_counter[PredictIndex] == 2 or saturating_counter[PredictIndex] == 3){
+                TakenPrediction = TAKEN;
+            }
+
+
+            
+            //saturating coutner modify
             if (TakenResult == "0")     //the TakenResult is 0 (not taken)
             {
+                if (saturating_counter[PredictIndex] == 0){
+                    //00
+                    saturating_counter[PredictIndex] = 0;
+                }
+                
+                if (saturating_counter[PredictIndex] == 1){
+                    //01
+                    saturating_counter[PredictIndex] = 0;
+                }
+                
+                if (saturating_counter[PredictIndex] == 2){
+                    //10
+                    saturating_counter[PredictIndex] = 0;
+                }
+                
+                if (saturating_counter[PredictIndex] == 3){
+                    //11
+                    saturating_counter[PredictIndex] = 2;
+                }
+                
 
             
             }
@@ -80,11 +122,27 @@ int main(){
             
             else                //the TakenResult is 1(Taken)
             {
-
-            
+                if (saturating_counter[PredictIndex] == 0){
+                    //00
+                    saturating_counter[PredictIndex] = 1;
+                }
                 
-            }
+                if (saturating_counter[PredictIndex] == 1){
+                    //01
+                    saturating_counter[PredictIndex] = 3;
+                }
+                
+                if (saturating_counter[PredictIndex] == 2){
+                    //10
+                    saturating_counter[PredictIndex] = 3;
+                }
+                
+                if (saturating_counter[PredictIndex] == 3){
+                    //11
+                    saturating_counter[PredictIndex] = 3;
+                }
             
+            }
             
             
             tracesout<< TakenPrediction << endl;  // Output whether this branch is predicted TAKEN or NOT TAKEN
@@ -94,9 +152,6 @@ int main(){
         tracesout.close();
     }
     else cout<< "Unable to open trace or traceout file ";
-    
-    
-    
     
     
     return 0;
